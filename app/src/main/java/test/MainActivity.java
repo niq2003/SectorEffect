c4df67aa3562249bf6bbac6182e324a267cc7e3d
niq2003 <niq2003@qq.com>
2017-06-19 16:51:17 +0800

    Update MainActivity.java
    update

--- app/src/main/java/test/MainActivity.java
+++ app/src/main/java/test/MainActivity.java
@@ -1,7 +1,13 @@
 package test;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.CharacterStyle;
+import android.text.style.UnderlineSpan;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -9,6 +15,7 @@ import com.test.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
         });
 
         final SectorTextView textView = (SectorTextView) findViewById(R.id.textview);
+        SpannableString spannable = new SpannableString("safasfasfsaffasfasfaffaffdsafjaksfjasfasf");
+        CharacterStyle span=new UnderlineSpan();
+        spannable.setSpan(span,5,10, Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+        textView.setText(spannable);
+        textView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
         findViewById(R.id.btn_text).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
